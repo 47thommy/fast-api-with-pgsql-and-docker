e78ee64c27dc804afd7b82243fed750f1415918f
47thommy <47thommy@gmail.com>
2024-09-10 17:59:25 +0300

    use the request body

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ async def search(username:str):
             return {"message":f"details for {username}"}
         else:
             return {"message":"no user with the specified username exist"}
+        
+        
+from pydantic import BaseModel
+
+class userSchema(BaseModel):
+    username:str
+    email:str
+        
+users =[]
+
+@app.post("/create")
+async def create_user(user_data:userSchema):
+    new_user = {
+        "username":user_data.username,
+        "email":user_data.email
+    }
+    users.append(new_user)
+    return {"message":f"user with email: { user_data.email} created succesfully"}
