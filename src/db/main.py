0a10f0a15c6d1c2a37820de978dc6454b2cc21bc
47thommy <47thommy@gmail.com>
2024-09-13 18:27:21 +0300

    create a dependency to get session

--- src/db/main.py
+++ src/db/main.py
@@ -1,9 +1,10 @@
-from sqlmodel import  text, SQLModel
+from sqlmodel import  SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine
 from src.books.models import Book
 from src.config import Config
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-print(Config.DATABASE_URL)
 
 engine = create_async_engine(
     url=Config.DATABASE_URL,
@@ -15,3 +16,10 @@ async def initdb():
 
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+        
+
+async def get_session()-> AsyncSession:
+    async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+    
+    async with async_session() as session:
+        yield session
