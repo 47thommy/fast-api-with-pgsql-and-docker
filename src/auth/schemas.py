428fdcb8aab63aa9c2fdfcdb34b5b06ac5bd555b
47thommy <47thommy@gmail.com>
2024-09-14 17:30:05 +0300

    add service for creating and getting user

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -4,6 +4,6 @@ from sqlmodel import Field
 class userCreateModel(BaseModel):
     first_name:str = Field(max_length=25, min_length=3)
     last_name:str = Field(max_length=25, min_length=3)
-    user_name:str = Field(max_length=8)
+    username:str = Field(max_length=8)
     email:str
     password:str = Field(min_length=8)
